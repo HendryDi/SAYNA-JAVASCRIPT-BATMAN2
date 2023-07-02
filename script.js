fc7cab005e77ab54751d713f14816af4cb33e410
HendryDi <h9ndri@gmail.com>
2023-07-02 22:26:28 +0300

    improve11

--- script.js
+++ script.js
@@ -231,3 +231,21 @@ document.querySelector("#submitBtn").addEventListener("click", function () {
   imgActive += 1;
   images[imgActive].classList.remove("hidden");
 });
+
+// pour scroller la page de haut en bas
+const scrollBtnUp = document.querySelector(".scroll img:first-of-type");
+const scrollBtnDown = document.querySelector(".scroll img:last-of-type");
+
+scrollBtnUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+scrollBtnDown.addEventListener("click", () => {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth",
+  });
+});
